55d911831eac9d1063c5e55d2af45b2917561dad
avinash mishra <personalavi11@gmail.com>
2025-11-06 15:12:25 +0530

    new commit

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,17 +1,25 @@
-import Car from './vehicle'
-import './App.css'
+import "./App.css"
+import Card from "./Card"
 
 function App() {
-  
-
-  return (
-    <>
-      <Car color="red"brand="BMW"/>
-      <h1>Testting Component Behaviour</h1>
-      <h2>testing</h2>
-      <Car color="black" brand="Lamborghini"/>
-    </>
+  return(
+    <div className="card-container">
+      <Card
+      image="https://marketplace.canva.com/EAGUhHGuQOg/1/0/1003w/canva-orange-and-blue-anime-cartoon-illustrative-novel-story-book-cover-WZE2VIj5AVQ.jpg"
+      title="React Basics"
+      description="Learn about components,props and state"
+      />
+       <Card
+      image="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/old-books-cover-design-template-528851dfc1b6ed275212cd110a105122_screen.jpg?ts=1698687093"
+      title="Java Script"
+      description="Master the language behind react"
+      />
+      <Card
+      image="https://miblart.com/wp-content/uploads/2024/01/main-6-scaled.jpg"
+      title="CSS Tricks"
+      description="Master the language behind react"
+      />
+    </div>
   )
 }
-
-export default App
+export default App;
